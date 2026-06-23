ee3044a29c7ab9b352561859a3e7edb0e07840d6
janeiroivar21-web <janeiroivar21@gmail.com>
2026-06-23 12:53:18 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,8 @@
-// firebase.js (MODERN SDK v12)
+// firebase.js (FIXED CLEAN VERSION)
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCaoJm7m20S2wyvg_qv3nRS2mn3ndzGAwU",
@@ -12,8 +13,9 @@ const firebaseConfig = {
   appId: "1:679819480296:web:1c57331b9dd4aa7e320d5d"
 };
 
-// Initialize Firebase
+// Initialize Firebase ONLY ONCE
 const app = initializeApp(firebaseConfig);
 
-// Export Auth (IMPORTANT)
+// Export services
 export const auth = getAuth(app);
+export const db = getFirestore(app);
