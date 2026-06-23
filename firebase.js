178689f17182b55d576f7fc3c11db9ccb19969cf
janeiroivar21-web <janeiroivar21@gmail.com>
2026-06-23 21:44:00 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,19 @@
-// firebase.js (FIXED CLEAN VERSION)
+// firebase.js
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCaoJm7m20S2wyvg_qv3nRS2mn3ndzGAwU",
-  authDomain: "m-shwari.firebaseapp.com",
-  projectId: "m-shwari",
-  storageBucket: "m-shwari.firebasestorage.app",
-  messagingSenderId: "679819480296",
-  appId: "1:679819480296:web:1c57331b9dd4aa7e320d5d"
+  apiKey: "AIzaSyAWTWSMZelhkMvhS8nyJQ6Jh5JCRoAKyJM",
+  authDomain: "m-shwari-abfe3.firebaseapp.com",
+  projectId: "m-shwari-abfe3",
+  storageBucket: "m-shwari-abfe3.firebasestorage.app",
+  messagingSenderId: "713338202870",
+  appId: "1:713338202870:web:505e67b052300ebd5d5ad7"
 };
 
-// Initialize Firebase ONLY ONCE
 const app = initializeApp(firebaseConfig);
 
-// Export services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
